17f7e9cf55d1a2e5d6ea5e314cb4838a916d85d2
Sovetkin, Max <m.sovetkin@itransition.com>
2022-08-01 13:34:44 +0300

    netatalk addon updated

--- netatalk/rootfs/etc/cont-init.d/afp.sh
+++ netatalk/rootfs/etc/cont-init.d/afp.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/with-contenv bashio
-addgroup "$(bashio::config 'username')"
-adduser -D -H -G "$(bashio::config 'username')" -s /bin/false "$(bashio::config 'username')"
-echo "$(bashio::config 'username'):$(bashio::config 'password')" | chpasswd
-sed -i'' -e "s,%AFP_USER%,$(bashio::config 'username'),g" /etc/afp.conf
+if id -u "$(bashio::config 'username')" >/dev/null 2>&1; then
+  exit 0
+else
+  echo "user does not exist"
+  addgroup "$(bashio::config 'username')"
+  adduser -D -H -G "$(bashio::config 'username')" -s /bin/false "$(bashio::config 'username')"
+  echo "$(bashio::config 'username'):$(bashio::config 'password')" | chpasswd
+  sed -i'' -e "s,%AFP_USER%,$(bashio::config 'username'),g" /etc/afp.conf
+fi
